1ba1fe05f30d8e07021524733c10376610b3b065
dashasheveleva <photodoshfy@gmail.com>
2021-09-20 16:50:35 -0700

    "v0.04"

--- demo/main.cpp
+++ demo/main.cpp
@@ -2,22 +2,59 @@
 #include <nlohmann/json.hpp>
 #include <student.hpp>
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 void print(const student_t& student, std::ostream& os) {
-  //...
-  if (student.debt.type() == typeid(std::nullptr_t)) {
-    os << "null";
-  } else if (student.debt.type() == typeid(std::string)) {
-    os << std::any_cast<std::string>(student.debt);
-  } else {
-    os
-        << std::any_cast<std::vector<std::string> >(student.debt).size()
-        << " items";
+
+  //name
+  os << "| " << setw(14) << left << std::string(student.name) << "| " ;
+
+  //group
+  if (student.group.type() == typeid(size_t)) {
+    os << setw(7) << left << std::any_cast<size_t>(student.group) << "| " ;
+  } else if (student.group.type() == typeid(std::string)) {
+    os << setw(7) << left << std::any_cast<std::string>(student.group) << "| ";
   }
+
+  //avg
+  if (student.avg.type() == typeid(size_t)) {
+    os << setw(5) << left << std::any_cast<size_t>(student.avg) << "| ";
+  } else if (student.avg.type() == typeid(double)) {
+    os << setw(5) << left << std::any_cast<double>(student.avg) << "| ";
+  } else if (student.avg.type() == typeid(std::string)) {
+    os << setw(5) << left << std::any_cast<std::string>(student.avg) << "| ";
+  }
+
+  //dept
+  if (student.debt.type() == typeid(std::nullptr_t)) {
+    os << setw(14)<<left << "null" << "|";
+  } else if (student.debt.type() == typeid(std::string)) {
+    os << setw(14)<<left << std::any_cast<std::string>(student.debt) << "|";
+  } else if (student.debt.type() == typeid(std::vector<std::string>)) {
+    os << setw(2) << std::any_cast<std::vector<std::string> >(student.debt).size()
+       << "items" << "       |";
+  }
+  os << endl;
+  os << "|---------------|--------|------|---------------|" << endl;
 }
 
 void print(const std::vector<student_t>& students, std::ostream& os) {
 
-  //...
+  //| name          | group  | avg  | debt          |
+  //|---------------|--------|------|---------------|
+  //| Ivanov Petr   | 1      | 4.25 | null          |
+  //|---------------|--------|------|---------------|
+  //| Sidorov Ivan  | 31     | 4.00 | C++           |
+  //|---------------|--------|------|---------------|
+  //| Pertov Nikita | IU8-31 | 3.33 | 3 items       |
+  //|---------------|--------|------|---------------|
+
+  os << "|" << setw(15)<<left << "name" << "|" << setw(8)<<left<< "group"
+     << "|" << setw(6) <<left<< "avg" << "|" << setw(15)<<left<< "dept" << "|"
+     << endl << "|---------------|--------|------|---------------|" << endl;
+
   for (auto const& student : students) {
     print(student, os);
   }
@@ -25,19 +62,35 @@ void print(const std::vector<student_t>& students, std::ostream& os) {
 
 int main() {
   std::string jsonPath= "/home/daria/CLionProjects/lab_01/students.json";
+  if (jsonPath.empty() == 0) {
+    throw std::runtime_error{"No argument containing file path"};
+  }
   std::ifstream file{jsonPath};
   if (!file) {
-    throw std::runtime_error{"unable to open json: " + jsonPath};
+    throw std::runtime_error{"Unable to open json: " + jsonPath};
   }
 
   json data;
   file >> data;
 
   std::vector<student_t> students;
+
+  if (data["items"].size() == 0) {
+    throw std::runtime_error{"No values in 'items'"};
+  }
+  if (data["_meta"]["count"].is_number() == 0) {
+    throw std::runtime_error{"Value 'count' mast be of type integer"};
+  }
+  if (data["items"].size() != data["_meta"]["count"]) {
+    throw std::runtime_error{"The value 'count' does not match the number of 'items'"};
+  }
+
   for (auto const& item : data.at("items")) {
-    auto student= item.get<student_t>();
+    student_t student;
+    from_json(item, student);
     students.push_back(student);
   }
-  //...
-  //print(students, std::cout);
+
+  print(students, cout);
+
 }
--- sources/student.cpp
+++ sources/student.cpp
@@ -3,23 +3,21 @@
 #include "student.hpp"
 
 void from_json(const json& j, student_t& s) {
-  s.name = get_name(j.at("group"));
+  s.name = get_name(j.at("name"));
   s.group = get_group(j.at("group"));
   s.avg = get_avg(j.at("avg"));
-  s.debt = get_group(j.at("debt"));
+  s.debt = get_debt(j.at("debt"));
 }
 
 auto get_name(const json& j) -> std::string {
   return j.get<std::string>();
 }
 
-auto get_debt(const json& j) -> std::any {
-  if (j.is_null())
-    return nullptr;
-  else if (j.is_string())
+auto get_group(const json& j) -> std::any {
+  if (j.is_string())
     return j.get<std::string>();
   else
-    return j.get<std::vector<std::string> >();
+    return j.get<std::size_t>();
 }
 
 auto get_avg(const json& j) -> std::any {
@@ -33,9 +31,11 @@ auto get_avg(const json& j) -> std::any {
     return j.get<std::size_t>();
 }
 
-auto get_group(const json& j) -> std::any {
-  if (j.is_string())
+auto get_debt(const json& j) -> std::any {
+  if (j.is_null())
+    return nullptr;
+  else if (j.is_string())
     return j.get<std::string>();
   else
-    return j.get<std::size_t>();
+    return j.get<std::vector<std::string> >();
 }
