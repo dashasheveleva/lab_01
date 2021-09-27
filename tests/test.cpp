0bc1bf2bd36023727868429d08140bcc01cf20bc
dashasheveleva <photodoshfy@gmail.com>
2021-09-27 05:37:31 -0700

    v0.09

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,7 @@
 #include <nlohmann/json.hpp>
 #include <student.hpp>
 #include <iostream>
+#include <typeinfo>
 
 TEST(FilePathExist, EmptyTest) {
   std::string jsonPath;
@@ -53,4 +54,74 @@ TEST(MetaCunt_LenItems, EmptyTest) {
   size_t var1= data["_meta"]["count"];
   size_t var2= students.size();
   EXPECT_EQ(var1, var2);
+}
+TEST(GroupType, EmptyTest) {
+  std::string jsonPath;
+  jsonPath= JPATH;
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data["items"]) {
+    student_t student;
+    from_json(item, student);
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool var = false;
+    if (student.group.type() == typeid(size_t)) {
+      var = true;
+    } else if (student.group.type() == typeid(std::string)) {
+      var = true;
+    }
+    EXPECT_EQ(var, true);
+  }
+}
+TEST(AvgType, EmptyTest) {
+  std::string jsonPath;
+  jsonPath= JPATH;
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data["items"]) {
+    student_t student;
+    from_json(item, student);
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool var = false;
+    if (student.avg.type() == typeid(size_t)) {
+      var = true;
+    } else if (student.avg.type() == typeid(double)) {
+      var= true;
+    } else if (student.avg.type() == typeid(std::string)) {
+      var = true;
+    }
+    EXPECT_EQ(var, true);
+  }
+}
+TEST(DeptType, EmptyTest) {
+  std::string jsonPath;
+  jsonPath= JPATH;
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data["items"]) {
+    student_t student;
+    from_json(item, student);
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool var = false;
+    if (student.debt.type() == typeid(std::nullptr_t)) {
+      var = true;
+    } else if (student.debt.type() == typeid(std::string)) {
+      var = true;
+    } else if (student.debt.type() == typeid(std::vector<std::string>)) {
+      var = true;
+    }
+      EXPECT_EQ(var, true);
+  }
 }
